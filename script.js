06214b150aaf14362bedf1e7d829e16d8b9ad12d
CookieCarter <28cweaver@go.dsdmail.net>
2024-10-11 17:02:44 -0600

    Added icons and script.

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-alert("derp")
+var percents = Array.prototype.slice.call(document.getElementsByTagName("h3"), 0);
+for (var i = 0; i < percents.length; i++) {
+	if (percents[i].innerText == "Assignments") {
+		percents[i] = 0;
+	} else {
+		percents[i] = parseInt(percents[i].innerText.slice(15,percents[i].innerText.length-18));
+	}
+}
+
+
+var sections = Array.prototype.slice.call(document.getElementsByClassName("list-group list-group-data"), 0);
+for (var i = 0; i < sections.length; i++) {
+	if (sections[i].className == "list-group list-group-data") {
+		sections[i] = 0;
+	} else {
+		sections[i] = sections[i].childElementCount;
+	}
+}
+
+var grades = Array.prototype.slice.call(document.getElementsByClassName("margin-left-10"), 0);
+grades = grades.filter(grade => grade.className === "margin-left-10");
+var newGrade = [];
+for (var i = 0; i < grades.length; i++) {
+	newGrade[i] = [0,0];
+}
+for (var i = 0; i < grades.length; i++) {
+	var index = sections[0];
+	var j = 0;
+	while(i >= index) {
+		index+=sections[j];
+		j++;
+	}
+	grades[i] = grades[i].innerText.split("/");
+	if (grades[i].length == 1) {
+		grades[i] = grades[i][0]/4;
+	} else if (grades[i][1] == 0) {
+		grades[i] = 0;
+	} else{
+		newGrade[i][1] = ((grades[i][1]*percents[j])/sections[j]/4);
+		grades[i] = grades[i][0]/grades[i][1]
+	}
+	newGrade[i][0] = ((grades[i]*percents[j])/sections[j]);
+}
+
+grades = Array.prototype.slice.call(document.getElementsByClassName("margin-left-10"), 0);
+grades = grades.filter(grade => grade.className === "margin-left-10");
+for (var i = 0; i < grades.length; i++) {
+	grades[i].innerHTML = grades[i].innerText + " (" + newGrade[i][0].toFixed(2) + "% / " + newGrade[i][1].toFixed(2) + "%)";
+}
